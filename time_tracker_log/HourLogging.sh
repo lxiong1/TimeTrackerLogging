eccfa66bc721c2f3e878c0c3b83eab0063fa8ea3
Lue Xiong <luexionglx@gmail.com>
2017-04-20 14:46:34 -0500

    .

--- time_tracker_log/HourLogging.sh
+++ time_tracker_log/HourLogging.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-pip install -U selenium
+pip install -U selenium==3.3.3
 pip install -U chromedriver
 chmod +x time_tracker_log.py
 python time_tracker_log.py
--- time_tracker_log/HoursLogging.bat
+++ time_tracker_log/HoursLogging.bat
@@ -1,5 +1,5 @@
 @echo off
 title Ebillity-Time Tracker
-pip install selenium
+pip install selenium==3.3.3
 pip install chromedriver
 python C:.\time_tracker_log.py %*
